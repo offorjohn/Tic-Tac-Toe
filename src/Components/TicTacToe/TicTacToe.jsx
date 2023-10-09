24cce6dcce9e1c95665101c25df08fc75c79183b
Offor john <your@email.com>
2023-10-09 21:42:30 +0100

    first commit

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './TicTacToe.css'
+
+const TicTacToe = () => {
+    return (
+        <div className='container'>
+          <h1 className='title'>Tic Tac Toe Game In <span>React</span></h1>
+           <div className='board'>
+
+            </div>
+             <button className="reset">Reset</button>
+        </div>
+    )
+}
+
+export default TicTacToe
